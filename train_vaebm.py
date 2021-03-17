3da327e55381980f752ce800cf3d6136133abe70
Pulkit Gopalani <gopalanipulkit1@gmail.com>
2021-03-17 10:46:57 +0530

    Langevin MCMC for epsilon

--- train_vaebm.py
+++ train_vaebm.py
@@ -1,30 +1,62 @@
 #Code for training VAEBM 
+import os 
+import sys
+
 import torch
 from torch.utils.data import DataLoader
 import numpy as np 
-import os 
+import tqdm 
 
-from disentangling_vae.disvae import * 
-from igebm_pytorch.model import *
-from igebm_pytorch.train import *
+from vae.disvae.training import Trainer
+from vae.disvae.utils.modelIO import load_model
+
+def sample_epsilon(vae_decoder, EBM, batch_size, latent_dim, sampling_steps, step_size):
+    pass
+    epsilon = torch.randn(batch_size,latent_dim)
+    
+    epsilon.requires_grad = True
+    vae_decoder.parameters.requires_grad = False
+    EBM.parameters.requires_grad = False
+
+    h_prob_dist = lambda eps: torch.exp(-EBM(vae_decoder(eps))) * torch.exp(-0.5 * (torch.linalg.norm(eps,dim=1) ** 2))
+
+    for _ in tqdm(range(sampling_steps)):
+        noise = torch.randn(batch_size,latent_dim)
+        
+        prob_out = h_prob_dist(epsilon)
+        prob_out.sum().backward()
+
+        epsilon.data.add_(noise,torch.sqrt(step_size))
+
+        epsilon.grad.data.clamp_(-0.01,0.01)
+
+        epsilon.data.add(-step_size / 2, epsilon.grad.data)
+        epsilon.grad.detach_()
+        epsilon.grad.zero_()
+        #epsilon.data.clamp_(0, 1)
+
+    return epsilon
+        
 
 
-def preprocess_data(dataset):
-    if dataset == 'mnist':
-        pass
-    elif dataset == 'cifar10':
-        pass
-    elif dataset == 'cifar100':
-        pass
+RES_DIR = './vae/results/'
 
-    return data
-
-DATASET = 'mnist'        #MNIST/CIFAR10/CIFAR100
-VAE = 'standard_vae'   #Standard VAE, beta-VAE, ...
+model_name = 'vae'
+model_dir = os.path.join(RES_DIR,model_name)
+vae_model = load_model(model_dir)
+vae_model.eval()
 
 
-def train_vaebm(vae=VAE,dataset=DATASET):
-    data = preprocess_data(dataset=dataset)
+
+
+
+
+
+
+
+    
+    
+    
 
 
 
