aa34ed1662304fbf7f00adc92b2cec3c81af269d
Pulkit Gopalani <gopalanipulkit1@gmail.com>
2021-03-17 12:05:29 +0530

    VAEBM training code added

--- train_vaebm.py
+++ train_vaebm.py
@@ -3,26 +3,50 @@ import os
 import sys
 
 import torch
-from torch.utils.data import DataLoader
 from torch.optim import Adam
+from torch.utils.data import DataLoader
 
+import tqdm 
 import numpy as np 
 import matplotlib.pyplot as plt
-import tqdm 
 
 from vae.disvae.training import Trainer
 from vae.disvae.utils.modelIO import load_model, load_metadata
 
 RES_DIR = './vae/results/'
 
-N_STEPS = 10
-STEP_SIZE = 8e-5
+LD_N_STEPS = 10
+LD_STEP_SIZE = 8e-5
 
 BATCH_SIZE = 32
 N_EPOCHS = 16
 ADAM_LR = 3e-5
 
-def langevin_sample_epsilon(vae_decoder, ebm, latent_dim, batch_size=BATCH_SIZE, sampling_steps=N_STEPS, step_size=STEP_SIZE):
+def load_ebm(model_dir):
+    """
+    Load the EBM model
+
+    Parameters--->
+        model_dir (str): location of model to be loaded
+
+    Returns--->
+        ebm_model (torch.nn.module): EBM model as required
+    """
+    pass
+
+def load_data(dataset):
+    """
+    Load the specified dataset for training.
+
+    Parameters--->
+        dataset (str): dataset specification ("CIFAR-10", "MNIST")
+
+    Returns--->
+        data_loader: torch dataloader object for the dataset
+    """
+    pass
+
+def langevin_sample_epsilon(vae_decoder, ebm, latent_dim, batch_size=BATCH_SIZE, sampling_steps=LD_N_STEPS, step_size=LD_STEP_SIZE):
     """
     Sample epsilon using Langevin dynamics based MCMC, 
     for reparametrizing negative phase sampling in EBM
@@ -47,7 +71,7 @@ def langevin_sample_epsilon(vae_decoder, ebm, latent_dim, batch_size=BATCH_SIZE,
 
     h_prob_dist = lambda eps: torch.exp(-ebm(vae_decoder(eps))) * torch.exp(-0.5 * (torch.linalg.norm(eps,dim=1) ** 2))
 
-    for _ in tqdm(range(sampling_steps)):
+    for _ in range(sampling_steps):
         noise = torch.randn(batch_size,latent_dim)
         
         prob_out = h_prob_dist(epsilon)
@@ -78,15 +102,15 @@ def train_vaebm(vae,ebm,dataset):
     Returns--->
         None
     """
-        
+
     vae.parameters.requires_grad = False
     ebm.parameters.requires_grad = True
     
-    data = DataLoader(dataset)
+    data = load_data(dataset)
 
     optimizer = Adam(params=ebm.parameters,lr=ADAM_LR)
 
-    for epoch in tqdm(range(N_EPOCHS)):
+    for epoch in range(N_EPOCHS):
         epoch_losses=[]
         for _,pos_image in tqdm(enumerate(data)):
             
@@ -105,17 +129,29 @@ def train_vaebm(vae,ebm,dataset):
             optimizer.zero_grad()
         
         epoch_losses.append(loss)
-        plt.plot(epoch,loss)
+        plt.plot(epoch+1,epoch_losses)
         plt.savefig('vaebm_training_loss.jpg')
 
+        torch.save(ebm.state_dict)
 
-def main(args):
 
+def main():
     model_name = 'vae'
     model_dir = os.path.join(RES_DIR,model_name)
 
     vae_model = load_model(model_dir)
     vae_model.eval()
+
+    ebm_model = load_ebm(model_dir)
+    ebm_model.train()
+
+    train_vaebm(vae_model,ebm_model,dataset)
+
+
+
+
+
+
 
 
 
