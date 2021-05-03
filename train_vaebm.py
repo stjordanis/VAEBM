bd28f07fc392a10a93cd1b1cc6dd72a1d10a1eb7
Pulkit Gopalani <gopalanipulkit1@gmail.com>
2021-05-03 15:47:24 +0530

    Fixed regularization

--- train_vaebm.py
+++ train_vaebm.py
@@ -78,7 +78,7 @@ def langevin_sample(vae, ebm, **kwargs):
 
     epsilon = torch.randn(kwargs['batch_size'],vae.latent_dim,device=device,requires_grad=True)
     
-    log_h_eps = lambda eps: ebm(vae.decoder(eps)) + 0.5 * (torch.linalg.norm(eps,dim=1) ** 2)
+    log_h_eps = lambda eps: ebm(vae.decoder(eps),eps) + 0.5 * (torch.linalg.norm(eps,dim=1) ** 2)
 
     for _ in range(kwargs['sample_steps']):
         noise = torch.randn(kwargs['batch_size'],vae.latent_dim,device=device)
@@ -152,15 +152,13 @@ def train_vaebm(vae, ebm, dataset, **kwargs):
                         sample_step_size=kwargs['sample_step_size']
                        )'''
                 
-                
-
                 with torch.no_grad():
                     neg_image = vae.decoder(epsilon)
 
-                pos_energy = ebm(pos_image)
-                neg_energy = ebm(neg_image)
+                pos_energy = ebm(pos_image,vae(pos_image)[2])
+                neg_energy = ebm(neg_image,epsilon)
                 energy_loss = pos_energy - neg_energy
-                energy_reg_loss =  pos_energy * 2 + neg_energy * 2
+                energy_reg_loss =  pos_energy ** 2 + neg_energy ** 2
                 spectral_norm_loss = ebm.spec_norm()
                 loss = (energy_loss + alpha_e * energy_reg_loss).mean() + alpha_n * spectral_norm_loss
 
